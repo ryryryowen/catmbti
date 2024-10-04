bcc9d5447347acd0ba06b963fa062a3b81350724
MacUser <macuser@MacUserui-MacBookPro.local>
2024-10-04 12:21:10 +0900

    kakaoedit

--- src/components/KakaoShareButton.jsx
+++ src/components/KakaoShareButton.jsx
@@ -2,13 +2,13 @@ import React, { useEffect } from "react";
 import { Button } from "react-bootstrap";
 const { Kakao } = window;
 
-const KakaoShareButton = () => {
+const KakaoShareButton = ({ data }) => {
   const url = "https://owencatmbti.netlify.app";
   const resultURL = window.location.href;
 
   useEffect(() => {
+    Kakao.cleanup();
     Kakao.init("994e75c4912337006aaf8ea50447be37");
-    console.log(Kakao.isInitialized());
   }, []);
 
   const shareKakao = () => {
@@ -16,13 +16,11 @@ const KakaoShareButton = () => {
       objectType: "feed",
       content: {
         title: "예비집사 판별기 결과",
-        description:
-          "예비집사님이 고양이를 키운다면 가장 잘 맞는 고양이는 아비나시안 입니다.",
-        imageUrl:
-          "https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
+        description: `예비집사님이 고양이를 키운다면 가장 잘 맞는 고양이는 ${data.name} 입니다.`,
+        imageUrl: `${url}${data.image}`,
         link: {
-          mobileWebUrl: "https://developers.kakao.com",
-          webUrl: "https://developers.kakao.com",
+          mobileWebUrl: resultURL,
+          webUrl: resultURL,
         },
       },
       buttons: [
--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -82,7 +82,7 @@ const Result = () => {
         </Desc>
         <ButtonGoup>
           <Button onClick={handleClickButton}>테스트 다시시작하기</Button>
-          <KakaoShareButton />
+          <KakaoShareButton data={resultData} />
         </ButtonGoup>
       </Contents>
     </Wrapper>
